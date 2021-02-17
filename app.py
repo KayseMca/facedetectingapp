b6a70ae88fe92257e67011370115b0f6b33c8b84
=KayseMca <=madadmca04@gmail.com>
2021-02-16 16:44:51 -0800

    complete

--- app.py
+++ app.py
@@ -181,6 +181,6 @@ def imageurl():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 33507))
     app.run('0.0.0.0',port= port)
     
