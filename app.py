6e0de0ad94d5e9768d4ad763d98abe3446b07f03
=KayseMca <=madadmca04@gmail.com>
2021-02-16 16:33:57 -0800

    update requirements

--- app.py
+++ app.py
@@ -182,5 +182,5 @@ def imageurl():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
-    app.run('0.0.0.0',port= port, debug=True)
+    app.run('0.0.0.0',port= port)
     
